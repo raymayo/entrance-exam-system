7e7b06f31a168030cdc7f6958990aa023f2b3b38
raymayo <ormillada.jr@gmail.com>
2025-03-28 06:32:10 +0800

    add Tailwind CSS integration and create Login component

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                text: "#081609",
+                background: "#f1faf2",
+
+
+            },
+        },
+    },
+    plugins: [],
+};
